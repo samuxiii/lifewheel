33c09bc1d572909161d8ffdaea65f6c1e639fe40
Samuel Gil <samuxiii@gmx.com>
2015-12-21 17:11:14 +0100

    Levels inside the PieSlice are operatives.
    It has been necessary to create a mechanism to allow notifications
    towards PieSlice from Slice when it is clicked.
    
    Details:
    Slice has registered a method of PieSlice that is called when the
    click event is received. Then PieSlice will know the level that has
    been clicked and can turnOn/turnOff slices according to the level.

--- lifewheel.py
+++ lifewheel.py
@@ -35,6 +35,7 @@ class PieSlice (object):
         offsetX = (x1 - x0)/(2 * self.levels)
         offsetY = (y1 - y0)/(2 * self.levels)
 
+        #creating slice levels into the pieslice
         for i in range(0, self.levels):
             sliceName = self.name + "slice" + str(i)
             #(i == 0) doesn't need to recalculate with offsets
@@ -47,31 +48,48 @@ class PieSlice (object):
             tempBbox = x0, y0, x1, y1
 
             print "Coord slice: %s" % (tempBbox,)
-            self.slices.append(Slice(tempBbox, start, self.extent, sliceName))
-            item = self.slices[i].draw(canvas)
-            canvas.tag_bind(item, "<1>", self.slices[i].mouse_click)
+            self.slices.append(Slice(tempBbox, start, self.extent, sliceName, level=i))
+            self.slices[i].draw(canvas)
+            self.slices[i].registerNotifier(self.turnOnLevel)
+
+    def turnOnLevel(self, canvas, level):
+        #TODO: levels are inverted... center is 9, external area is 0
+        for i in range(0, self.levels):
+            self.slices[i].turnOff(canvas)
+        for i in range(level, self.levels):
+            self.slices[i].turnOn(canvas)
 
 
 class Slice (object):
-    def __init__(self, bbox, start, extent, name=None):
+    def __init__(self, bbox, start, extent, name, level):
         print "Init Slice %s" % (name)
         self.coords = bbox
         self.start = start
         self.extent = extent
         self.name = name
-        self.green = False
+        self.level = level
+        self.notifier = None #reference to parent method
 
     def draw(self, canvas, outline="black", fill="white"):
-        return canvas.create_arc(self.coords, start=self.start, extent=self.extent, tags=self.name, outline=outline, fill=fill)
+        item = canvas.create_arc(self.coords, start=self.start, extent=self.extent, tags=self.name, outline=outline, fill=fill)
+        canvas.tag_bind(item, "<1>", self.mouse_click)
 
     def mouse_click(self, event):
         print "I got a mouse click (%s)" % self.name
-        if self.green:
-                self.green = False
-                event.widget.itemconfig(self.name, fill="blue") #change color
-        else:
-                self.green = True
-                event.widget.itemconfig(self.name, fill="green")
+        #call to notifier() == PieSlice.turnOnLevel(canvas, level)
+        self.notifier(event.widget, self.level)
+
+    def registerNotifier(self, func):
+        #this function will be use to notify parent PieSlice object
+        self.notifier = func 
+
+    def turnOn(self, canvas):
+        self.green = False
+        canvas.itemconfig(self.name, fill="green")
+
+    def turnOff(self, canvas):
+        self.green = True
+        canvas.itemconfig(self.name, fill="white")
 
 
 if __name__ == "__main__":
